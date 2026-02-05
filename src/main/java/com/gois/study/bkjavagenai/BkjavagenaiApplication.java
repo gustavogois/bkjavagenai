8c8c9ff84d1c482ca8559e09729473cce657d8c8
nableggois <gustavo.gois@n-able.com>
2026-02-05 15:18:16 +0000

    Initial commit

---
+++ src/main/java/com/gois/study/bkjavagenai/BkjavagenaiApplication.java
@@ -0,0 +1,13 @@
+package com.gois.study.bkjavagenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkjavagenaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BkjavagenaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gois/study/bkjavagenai/BkjavagenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gois.study.bkjavagenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkjavagenaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
